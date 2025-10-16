5a8d2a2a087118a83e3e70ba1e46434b2394516f
iamSamDiak <thediakarthur@gmail.com>
2025-10-17 01:37:20 +0200

    first commit

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from gui import MainWindow
 from PyQt5.QtWidgets import QApplication
 import sys
 
+__version__ = "1.0.0"
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
+    window.setWindowTitle(f"DOCX to PPTX Converter v{__version__}")
     window.show()
     sys.exit(app.exec_())
